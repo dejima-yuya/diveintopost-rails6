119a95d9f0b1006d45646f57e1503afccb316eae
dejima-yuya <jimajapan33@yahoo.co.jp>
2023-07-23 20:48:22 +0900

    チームのリーダー以外のメンバーが、チームの編集やチームメンバーの削除が可能であったため、修正

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -15,7 +15,11 @@ class TeamsController < ApplicationController
     @team = Team.new
   end
 
-  def edit; end
+  def edit
+    if current_user != @team.owner
+      redirect_to @team, notice: 'チームのリーダーしかチームを編集できません'
+    end
+  end
 
   def create
     @team = Team.new(team_params)
