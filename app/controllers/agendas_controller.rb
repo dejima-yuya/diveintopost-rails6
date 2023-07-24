56157d4e5081713e51b13e4a618eca321a89d473
dejima-yuya <jimajapan33@yahoo.co.jp>
2023-07-24 19:44:53 +0900

    アジェンダの削除機能を実装

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -1,5 +1,5 @@
 class AgendasController < ApplicationController
-  # before_action :set_agenda, only: %i[show edit update destroy]
+  before_action :set_agenda, only: %i[show edit update destroy]
 
   def index
     @agendas = Agenda.all
@@ -18,6 +18,14 @@ class AgendasController < ApplicationController
       redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda')
     else
       render :new
+    end
+  end
+
+  def destroy
+    if @agenda.destroy
+      redirect_to dashboard_path, notice: 'アジェンダが削除されました！'
+    else
+      redirect_to dashboard_path, notice: 'なんらかの原因で、アジェンダは削除できませんでした'
     end
   end
 
